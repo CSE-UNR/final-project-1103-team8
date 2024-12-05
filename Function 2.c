c5afea51c7358ddbb9e41ef9540e072782a10701
1103-Chen-Daniel <zheyuan.chen04@gmail.com>
2024-12-05 05:09:49 +0000

    Add files via upload

---
+++ Function 2.c
@@ -0,0 +1,25 @@
+// Team-8 Function 2
+
+#include <stdio.h>
+
+void InputFunction(int rowIndex, int columnIndex, char sheet[][columnIndex]){
+	for(int row = 0; row <= rowIndex; row++){
+		for(int column = 0; column <= columnIndex; column++){
+		// need to know how much word of each types
+			if(row = 0){
+				printf("Enter a Adjective:\n");
+				scanf(" %c", &sheet[row][column]);
+				}
+			if(row = 1){
+				printf("Enter a Noun:\n");
+				scanf(" %c", &sheet[row][column]);
+				}
+			if(row = 2){
+				printf("Enter a Verb:\n");
+				scanf(" %c", &sheet[row][column]);
+				}
+				}
+		printf("\n");
+		}
+		}
+				
