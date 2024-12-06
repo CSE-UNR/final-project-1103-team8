2291c24ed5575c0d752b04c2dc6c043a09153f62
1103-Chen-Daniel <zheyuan.chen04@gmail.com>
2024-12-06 00:28:29 +0000

    Add files via upload

---
+++ Function 7.c
@@ -0,0 +1,16 @@
+//Team-8 Function 7
+//Save Function
+#define rowIndex 100
+#define columnIndex 100
+
+void copyFunction(int rowIndex, int columnIndex, char originalstring[][columnIndex]){
+
+char *originalstring[rowIndex][columnIndex];
+
+for(int row = 0; row <= rowIndex; row++){
+	for(int column = 0; column <= columnIndex; column++){
+	fscanf(filepointer, %c, originalstring[row][column]);
+	}
+	}
+	
+	}
