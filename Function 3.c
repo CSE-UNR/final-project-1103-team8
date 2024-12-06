1c2be658cdf167e299f565aeb2f980b96a780169
1103-Chen-Daniel <zheyuan.chen04@gmail.com>
2024-12-06 00:43:21 +0000

    Add files via upload

---
+++ Function 3.c
@@ -0,0 +1,16 @@
+//Team-8 Function 2
+//Copy Function
+
+#include <stdio.h>
+void ANV_Function(int rowIndex, char ANV_string[rowIndex]){
+
+char ANV_string[rowIndex]
+
+int a = 0;
+for(int i =0; i <= columnIndex; i++){
+	if(i % 2 = 1){
+	ANV_string[a] = originalstring[i][0];
+	a++;
+	}
+	}
+}
