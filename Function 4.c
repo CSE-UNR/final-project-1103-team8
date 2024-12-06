dd61005340ec610896e72dab8eaf74f96d875ca8
1103-Chen-Daniel <zheyuan.chen04@gmail.com>
2024-12-06 00:57:42 +0000

    Add files via upload

---
+++ Function 4.c
@@ -0,0 +1,21 @@
+
+
+void(int columnIndex, char AVN_string[columnIndex]){
+
+char AVN_string[columnIndex];
+
+for(int i = 0; i <= columnIndex; i++){
+	if(AVN_string[i] = "N"){
+		printf("Enter a None:\n");
+		scanf(" %s", &AVN_string[i]);
+		}
+	if(AVN_string[i] = "A"){
+		printf("Enter a Adjective:\n");
+		scanf(" %s", &AVN_string[i]);
+		}
+	if(AVN_string[i] = "V"){
+		printf("Enter a Verb:\n");
+		scanf(" %s", &AVN_string[i]);
+		}
+		}
+}
