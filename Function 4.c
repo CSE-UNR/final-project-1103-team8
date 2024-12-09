0d5495683ba4c93306c643dea19523dafa4bfcc8
1103-Chen-Daniel <zheyuan.chen04@gmail.com>
2024-12-09 19:15:00 +0000

    Update Function 4.c

--- Function 4.c
+++ Function 4.c
@@ -3,19 +3,19 @@
 void(int columnIndex, char AVN_string[columnIndex]){
 
 char AVN_string[columnIndex];
-
-for(int i = 0; i <= columnIndex; i++){
-	if(AVN_string[i] = "N"){
+	
+switch(AVN_string[i]){
+	case N:
 		printf("Enter a None:\n");
 		scanf(" %s", &AVN_string[i]);
-		}
-	if(AVN_string[i] = "A"){
+		break;
+	case A:
 		printf("Enter a Adjective:\n");
 		scanf(" %s", &AVN_string[i]);
-		}
-	if(AVN_string[i] = "V"){
+		break;
+	case V:
 		printf("Enter a Verb:\n");
 		scanf(" %s", &AVN_string[i]);
-		}
+		break;
 		}
 }
