c14e0fee2011abc1ed9d54282234dac305978d25
1103-Benjamin-RhodeAly <rhodealyb@unr.edu>
2024-12-10 23:07:22 +0000

    madlibs.c

--- madlibs.c
+++ madlibs.c
@@ -1,17 +1,18 @@
-//Author: Rhode Aly Benjamin, Perry, Daniel
+//Author: Rhode Aly Benjamin, Perry Guerrero, Daniel Chen
 //Date: 12/10/2024
 //Final Project
 
 #include <stdio.h>
 #define FILE1 "madlib1.txt"
-#define STR_CAP 100
+#define rowIndex 100
+#define columnIndex 100
 
-void input(FILE* fp, char adjective[], char noun[], char verb[]);
+void copyFunction(int rowIndex, int columnIndex, char originalString[][columnIndex]);
+void ANV_Function(int rowIndex, char ANV_string[rowIndex]);
+void userInput(int columnIndex, char AVN_string[columnIndex]);
 void display(char adjective[], char noun[], char verb[]);
  
 int main(){
-	
-	char adjective[STR_CAP], noun[STR_CAP], verb[STR_CAP];
 	
 	FILE *fp;
 	fp = fopen(FILE1, "w");
@@ -19,7 +20,9 @@ int main(){
 		printf("can't open file\n");
 		return 0;
 	}
-	input(fp, adjective, noun, verb);
+	copyFunction(rowIndex, columnIndex, originalString[][columnIndex]);
+	ANV_Function(rowIndex, ANV_string[rowIndex]);
+	userInput(columnIndex, AVN_string[columnIndex]);
 	display(adjective, noun, verb);
 	
 	fclose(fp);
@@ -27,6 +30,55 @@ int main(){
 	return 0;
 	}
 
-void input(FILE* fp, char adjective[], char noun[], char verb[]){
+void copyFunction(int rowIndex, int columnIndex, char originalString[][columnIndex]){
 
+	char *originalstring[rowIndex][columnIndex];
+
+	for(int row = 0; row <= rowIndex; row++){
+		for(int column = 0; column <= columnIndex; column++){
+		fscanf(filepointer, "%c", originalstring[row][column]);
+			}
+		}
+	}
+	
+void ANV_Function(int rowIndex, char ANV_string[rowIndex]){
+
+	char ANV_string[rowIndex]
+
+	int a = 0;
+	for(int i =0; i <= columnIndex; i++){
+		if(i % 2 = 1){
+			ANV_string[a] = originalstring[i][0];
+			a++;
+		}
+	}
+}
+
+void userInput(int columnIndex, char AVN_string[columnIndex]){
+
+	char AVN_string[columnIndex];
+	
+	switch(AVN_string[i]){
+	case N:
+		printf("Enter a None:\n");
+		scanf(" %s", &AVN_string[i]);
+		break;
+	case A:
+		printf("Enter a Adjective:\n");
+		scanf(" %s", &AVN_string[i]);
+		break;
+	case V:
+		printf("Enter a Verb:\n");
+		scanf(" %s", &AVN_string[i]);
+		break;
+		}
+}
+	
+void display(int rowIndex, int columnIndex, char originalString[][columnIndex]){
+	for(int line = 0; line < rowIndex; line++){
+		for(int value = 0; value < columnIndex; value++){
+			printf("%s", originalString[][columnIndex]);
+			}
+		printf("\n");
+		}
 	}
