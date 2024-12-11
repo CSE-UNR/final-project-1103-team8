d7d9d6e69193acd0cd133c2ba47170b90931e3b8
1103-Guerrero-Perry <pguerrero.school@gmail.com>
2024-12-11 00:41:06 +0000

    Add files via upload

---
+++ wordReplace.c
@@ -0,0 +1,31 @@
+//Perry Guerrero
+//
+//madlib.c word Replacer
+
+/* IN MAIN
+
+wordReplace()
+
+
+*/
+
+
+
+void wordReplace(int row, int rowIndex, originalString[][columnindex], ANV_string[rowIndex])
+{
+	for (int row = 0; row <= rowIndex; row++)
+	{
+		int a = 0;
+		for(int i =0; i <= columnIndex; i++)
+		{
+			if(i % 2 = 1)
+			{
+			originalstring[i][0] = ANV_string[a];
+			a++;
+			}
+		}
+	}
+}
+
+
+
