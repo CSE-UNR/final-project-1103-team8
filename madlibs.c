e06ad482fc0fdcadd073cb20eeb9dfb9cb8fe287
1103-Benjamin-RhodeAly <rhodealyb@unr.edu>
2024-12-11 00:56:43 +0000

    madlibs.c

--- madlibs.c
+++ madlibs.c
@@ -10,6 +10,7 @@
 void copyFunction(FILE* fp, int rowIndex, int columnIndex, char originalString[][COLUMNINDEX]);
 void ANV_Function(int rowIndex, char ANV_string[rowIndex], char originalString[][COLUMNINDEX]);
 void userInput(int rowIndex, char ANV_string[rowIndex]);
+void wordReplace(int rowIndex, char originalString[][COLUMNINDEX], char ANV_string[ROWINDEX]);
 void display(int rowIndex, int columnIndex, char originalString[][COLUMNINDEX]);
  
 int main(){
@@ -27,7 +28,7 @@ int main(){
 	copyFunction(fp, rowIndex, columnIndex, originalString);
 	ANV_Function(rowIndex, ANV_string, originalString);
 	userInput(rowIndex, ANV_string);
-	
+	wordReplace(rowIndex, originalString, ANV_string);
 	display(rowIndex, columnIndex, originalString);
 	
 	fclose(fp);
@@ -37,11 +38,11 @@ int main(){
 
 void copyFunction(FILE* fp, int rowIndex, int columnIndex, char originalString[][COLUMNINDEX]){
 
-	char *originalstring[ROWINDEX][COLUMNINDEX];
-
+	//while loop 
+	//you might want to return the number of rows in the file
 	for(int row = 0; row <= rowIndex; row++){
 		for(int column = 0; column <= columnIndex; column++){
-		fscanf(fp, "%c", originalstring[row][column]);
+		fgets(fp, "%c", originalString[row][column]);
 			}
 		}
 	}
@@ -50,6 +51,7 @@ void ANV_Function(int rowIndex, char ANV_string[ROWINDEX], char originalString[]
 
 	int a = 0;
 	for(int i =0; i <= rowIndex; i++){
+	//update this if condition
 		if(i % 2 == 1){
 			ANV_string[a] = originalString[i][0];
 			a++;
@@ -60,6 +62,7 @@ void ANV_Function(int rowIndex, char ANV_string[ROWINDEX], char originalString[]
 void userInput(int rowIndex, char ANV_string[ROWINDEX]){
 	
 	int i;
+	//add loop
 	switch(ANV_string[i]){
 		case 'N':
 			printf("Enter a Noun:\n");
@@ -75,7 +78,22 @@ void userInput(int rowIndex, char ANV_string[ROWINDEX]){
 		break;
 		}
 }
-	
+void wordReplace(int rowIndex, char originalString[][COLUMNINDEX], char ANV_string[ROWINDEX])
+{
+	for (int row = 0; row <= rowIndex; row++)
+	{
+		int a = 0;
+		for(int i = 0; i <= rowIndex; i++)
+		{
+			//change if condition
+			if(i % 2 == 1)
+			{
+			originalString[i][0] = ANV_string[a];
+			a++;
+			}
+		}
+	}
+}	
 void display(int rowIndex, int columnIndex, char originalString[][COLUMNINDEX]){
 	for(int line = 0; line < rowIndex; line++){
 		for(int value = 0; value < columnIndex; value++){
